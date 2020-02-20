dbd5ff2c3b2882ea56ab063f4cf017076d917be8
Maria Palacios <imarip09@gmail.com>
2020-02-20 10:40:43 -0600

    fixed field length functions

--- src/validators.js
+++ src/validators.js
@@ -1,7 +1,8 @@
 export const required = value => value ? undefined : 'Required';
-export const minLength = min => value => value && value < min ? `Must be at least ${min} characters` : undefined;
+export const nonEmpty = value => value.trim !== '' ? undefined : 'Cannot be empty';
+export const minLength = min => value => value && value.length < min ? `Must be at least ${min} characters` : undefined;
 export const usernameMinLength = minLength(3);
 export const pwMinLength = minLength(8);
-export const maxLength = max => value => value && value > max ? `Must be ${max} characters or less` : undefined;
+export const maxLength = max => value => value && value.length > max ? `Must be ${max} characters or less` : undefined;
 export const pwMaxLength = maxLength(72);
-export const isTrimmed = value => value.trim() === value ? undefined : 'Cannot start or end with spaces';
+export const isTrimmed = value => value.trim() === value ? undefined : 'Cannot start or end with spaces'; 
