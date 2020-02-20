6af8eb83068d7c089a32c964198a1d54e67e54f7
Maria Palacios <imarip09@gmail.com>
2020-02-20 12:01:46 -0600

    reducers for users

---
+++ src/reducers/users.js
@@ -0,0 +1,20 @@
+import { USERS_REQUEST, USERS_SUCCESS, USERS_ERROR } from '../actions/users';
+
+const initialState = {
+  user: '',
+  isLoading: false, 
+  error: null
+};
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case USERS_REQUEST: 
+      return { ...state, isLoading: true, error: null };
+    case USERS_SUCCESS:
+      return { ...state, user: action.user, isLoading: false, error: null };
+    case USERS_ERROR:
+      return { ...state, isLoading: false, error: action.error };
+    default: 
+      return state;
+  }
+};
