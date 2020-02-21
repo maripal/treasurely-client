b6b007841f3990aea644e6827307b02a183e1d44
Maria Palacios <imarip09@gmail.com>
2020-02-21 15:04:34 -0600

    updated file

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Field, reduxForm, focus } from 'redux-form';
+import Input from './Input';
+import { login } from '../actions/auth';
+import { required, nonEmpty } from '../validators';
+import './LoginForm.css';
 
-const LoginForm = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+export class LoginForm extends React.Component {
+  onSubmit = values => {
+    const { username, password } = values;
+    return this.props.dispatch(login(username, password));
+  }
+
+  render() {
+    let errorMessage;
+    if (this.props.error) {
+      errorMessage = (
+        <div>
+          {this.props.error}
+        </div>
+      );
+    }
+
+    return (
+      <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))} className="login-form">
+        {errorMessage}
+        <Field 
+          name="username"
+          type="text"
+          component={Input}
+          label="Username"
+          validate={[required, nonEmpty]}
+        />
+        <Field 
+          name="password"
+          type="text"
+          component={Input}
+          label="Password"
+          validate={[required, nonEmpty]}
+        />
+      </form>
+    );
+  }
 };
 
-export default Login;
+export default reduxForm ({
+  form: 'login',
+  onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
+})(LoginForm);
