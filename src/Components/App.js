de1c29a212e77aa75182bc012d8644beada5ed93
Maria Palacios <imarip09@gmail.com>
2020-02-21 15:03:21 -0600

    connected to redux state

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { refreshAuthToken } from '../actions/auth';
+
 import Nav from './Nav';
 import LandingPage from './LandingPage';
-import LoginPage from './LoginPage';
 import SignUpPage from './SignUpPage';
 import HomePage from './HomePage';
 import './App.css';
 
 class App extends React.Component {
+  componentDidUpdate (prevProps) {
+    if(!prevProps.loggedIn && this.props.loggedIn) {
+      this.startPeriodicRefresh();
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+      this.stopPeriodicRefresh();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()), 60 * 60 * 1000
+    );
+  }
+
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
+    }
+    clearInterval(this.refreshInterval);
+  }
+
   render() {
     return (
       <div>
         <Router>
           <Nav />
           <Route path="/" exact component={LandingPage} />
-          <Route path="/login" exact component={LoginPage} />
           <Route path="/signup" exact component={SignUpPage} />
           <Route path="/home" exact component={HomePage} />
         </Router>
       </div>
-    )
+    );
   }
-}
+};
 
-export default App;
+const mapStateToProps = state => ({
+  hasAuthToken: state.auth.authToken !== null,
+  loggedIn: state.auth.currentUser !== null
+});
+
+export default withRouter(connect(mapStateToProps)(App));
