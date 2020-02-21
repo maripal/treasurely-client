2ba789df15b5787ba6ad0377afe43494ece7f622
Maria Palacios <imarip09@gmail.com>
2020-02-21 10:56:23 -0600

    added auth and users reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import totalReducer from './totalReducer';
 import listItemsReducer from './listItemsReducer';
+import authReducer from './auth';
+import usersReducer from './users';
 
 export default combineReducers({
   total: totalReducer,
   items: listItemsReducer,
-  form: formReducer
+  form: formReducer,
+  auth: authReducer,
+  users: usersReducer
 });
