8f6f85a827687e116aae24efed53ab5d1e913c58
Maria Palacios <imarip09@gmail.com>
2020-02-26 17:59:02 -0600

    fixed props

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -6,9 +6,9 @@ const Item = props => {
 
   return (
     <div>
-      <h4 className="item-name">{props.item.name}</h4>
-      <p className="price">${props.item.price.toFixed(2)}</p>
-      <button className={`item-action-button ${props.className}`} onClick={() => props.buyClick(props.item.price, props.id)}>Buy</button>
+      <h4 className="item-name">{props.name}</h4>
+      <p className="price">${props.price.toFixed(2)}</p>
+      <button className={`item-action-button ${props.className}`} onClick={() => props.buyClick(props.price, props.id)}>Buy</button>
       <button className="item-action-button" onClick={() => props.editClick(props.id)}>Edit</button>
       <button className="item-action-button" onClick={() => props.clickDelete(props.id)}>Delete</button>
     </div>
