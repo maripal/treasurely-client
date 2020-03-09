f23abd6d20491baa64f27fc152c95dc361be211f
Maria Palacios <imarip09@gmail.com>
2020-03-09 14:39:31 -0500

    added auth actions and reducers

---
+++ src/actions/auth.js
@@ -0,0 +1,96 @@
+import jwtDecode from 'jwt-decode';
+import { SubmissionError } from 'redux-form';
+
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from './utils';
+import { saveAuthToken, clearAuthToken } from '../local-storage';
+
+export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
+export const CLEAR_AUTH = 'CLEAR_AUTH';
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_ERROR = 'AUTH_ERROR';
+
+export const setAuthToken = authToken => {
+  return {
+    type: SET_AUTH_TOKEN,
+    authToken
+  };
+};
+
+export const clearAuth = () => {
+  return {
+    type: CLEAR_AUTH
+  };
+};
+
+export const authRequest = () => {
+  return {
+    type: AUTH_REQUEST
+  };
+};
+
+export const authSuccess = currentUser => {
+  return {
+    type: AUTH_SUCCESS,
+    currentUser
+  };
+};
+
+export const authError = error => {
+  return {
+    type: AUTH_ERROR,
+    error
+  };
+};
+
+const storeAuthInfo = (authToken, dispatch) => {
+  const decodedToken = jwtDecode(authToken);
+  dispatch(setAuthToken(authToken));
+  dispatch(authSuccess(decodedToken.user));
+  saveAuthToken(authToken);
+};
+
+export const login = (username, password) => (dispatch, getState) => {
+  dispatch(authRequest());
+  console.log(`this is login state: ${JSON.stringify(getState())}`)
+  return (
+    fetch(`${API_BASE_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(({ authToken }) => storeAuthInfo(authToken, dispatch))
+    .catch(err => {
+      const { code } = err;
+      const message = code === 401 ? 'Incorrect username or password' : 'Unable to log in, please try again';
+      dispatch(authError(err));
+      return Promise.reject(
+        new SubmissionError({ _error: message })
+      );
+    })
+  );
+};
+
+export const refreshAuthToken = () => (dispatch, getState) => {
+  dispatch(authRequest);
+  const authToken = getState().auth.authToken;
+  return fetch(`${API_BASE_URL}/auth/refresh`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${authToken}`
+    }
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(({ authToken }) => storeAuthInfo(authToken, dispatch))
+    .catch(err => {
+      dispatch(authError(err));
+      dispatch(clearAuth());
+      clearAuthToken(authToken);
+    })
+};
---
+++ src/reducers/auth.js
@@ -0,0 +1,25 @@
+import { SET_AUTH_TOKEN, CLEAR_AUTH, AUTH_REQUEST, AUTH_SUCCESS, AUTH_ERROR } from '../actions/auth';
+
+const initialState = {
+  authToken: null,
+  currentUser: null,
+  isLoading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case SET_AUTH_TOKEN:
+      return { ...state, authToken: action.authToken };
+    case CLEAR_AUTH:
+      return { ...state, authToken: null, currentUser: null };
+    case AUTH_REQUEST:
+      return { ...state, isLoading: true, error: null };
+    case AUTH_SUCCESS:
+      return { ...state, isLoading: false, currentUser: action.currentUser };
+    case AUTH_ERROR:
+      return { ...state, isLoading: false, error: action.error };
+    default:
+      return state;
+  };
+};
