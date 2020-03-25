cd52f751e9e021814b9304164a43cf501715ff05
Maria Palacios <imarip09@gmail.com>
2020-03-25 16:39:15 -0500

    added actions for update total

--- src/reducers/totalReducer.js
+++ src/reducers/totalReducer.js
@@ -1,15 +1,32 @@
-import { ADD_TO_TOTAL, MINUS_TOTAL } from '../actions';
+import { 
+  UPDATE_TOTAL_SUCCESS, 
+  INCREASE_TOTAL, 
+  UPDATE_TOTAL_REQUEST, 
+  UPDATE_TOTAL_ERROR,
+  MINUS_TOTAL 
+} from '../actions/index';
 
-// const initialState = {
-//   totalSavings: 0
-// }
+const initialState = {
+  total: 0,
+  isLoading: false,
+  error: null
+};
 
-export default (state = 0, action) => {
+export default (state = initialState, action) => {
   switch(action.type) {
-    case ADD_TO_TOTAL:
-      return state + action.amount;
-    case MINUS_TOTAL:
-      return state - action.amount;
+    // case GET_TOTAL:
+    //   return {...state, state: action.amount}
+    case UPDATE_TOTAL_REQUEST: 
+      console.log(state)
+      return {...state, isLoading: !false};
+    case UPDATE_TOTAL_SUCCESS:
+    case INCREASE_TOTAL:
+      console.log(state)
+      return {...state, total: action.amount};
+    case UPDATE_TOTAL_ERROR: 
+      return {...state, isLoading: false, error: action.error}
+/*     case MINUS_TOTAL:
+      return state - action.amount; */
     default:
       return state;
   }
