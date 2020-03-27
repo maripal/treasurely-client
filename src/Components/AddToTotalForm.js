3a23e1fee4c597bb5223f822ab8663216fd76290
Maria Palacios <imarip09@gmail.com>
2020-03-27 10:27:41 -0500

    updated input name to match api

--- src/Components/AddToTotalForm.js
+++ src/Components/AddToTotalForm.js
@@ -1,56 +1,92 @@
 import React from 'react';
+import { reduxForm, Field, focus, reset } from 'redux-form';
 import { connect } from 'react-redux';
-import { addToTotal, minusTotal } from '../actions';
+import { increaseTotal, minusTotal } from '../actions';
+import Input from './Input';
 import './AddToTotalForm.css';
 
 class AddToTotalForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      amountInput: 0
-    };
-  };
 
-  onChange = event => {
-    this.setState ({ amountInput: event.target.value })
-  }
-
-  addMoneySubmit = event => {
-    event.preventDefault();
-
-    let numAmountInput = Number(this.state.amountInput);
-    this.props.addToTotal(numAmountInput);
-    this.setState ({ amountInput: 0 })
-  } 
-
-  subtractMoneySubmit = event => {
-    event.preventDefault();
-
-    let numAmountInput = Number(this.state.amountInput);
-    this.props.minusTotal(numAmountInput);
-    this.setState({ amountInput: 0 })
+  onSubmit = value => {
+    console.log(value);
+    return this.props.dispatch(increaseTotal(value));
   }
 
   render() {
+    let successMessage;
+    if (this.props.submitSucceeded) {
+      successMessage = (
+        <div>
+          Total has been added!
+        </div>
+      );
+    }
+
+    let errorMessage;
+    if (this.props.error) {
+      errorMessage = (
+        <div>
+          {this.props.error}
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <form className="total-form">
-          <input 
-            name="total"
-            type="text"
-            pattern="\d+(\.\d{0,2})?"
-            onChange={this.onChange}
-            placeholder="Add amount"
-            value={this.state.amountInput}
-            className="total-input"
-            required
-          />
-          <button className="add-button" type="submit" onClick={this.addMoneySubmit}>+</button>
-          <button className="minus-button" type="submit" onClick={this.subtractMoneySubmit}>-</button>
-        </form>
-      </div>
+      //  <div>
+      //   <form className="total-form">
+      //     <input 
+      //       name="total"
+      //       type="text"
+      //       pattern="\d+(\.\d{0,2})?"
+      //       onChange={this.onChange}
+      //       placeholder="Add amount"
+      //       value={this.state.amountInput}
+      //       className="total-input"
+      //       required
+      //     />
+      //     <button className="add-button" type="submit" onClick={this.addMoneySubmit}>+</button>
+      //     <button className="minus-button" type="submit" onClick={this.subtractMoneySubmit}>-</button>
+      //   </form>
+      // </div> 
+
+      <form
+       onSubmit={this.props.handleSubmit(value => this.onSubmit(value))}
+       className="total-form"
+      >
+        {successMessage}
+        {errorMessage}
+        <Field 
+          name="totalSavings"
+          type="number"
+          component={Input}
+          label="Amount"
+          parse={val => Number(val)}
+          required
+        />
+        <button 
+          className="add-button" 
+          type="submit"
+          disabled={this.props.pristine || this.props.submitting}
+        >
+          +
+        </button>
+        <button
+          className="minus-button"
+          type="submit"
+          disabled={this.props.pristine || this.props.submitting}
+        >
+          -
+        </button>
+      </form>
     );
   }
 };
 
-export default connect(null, { addToTotal, minusTotal })(AddToTotalForm);
+export default reduxForm ({
+  form: 'updateTotal',
+  onSubmitFail: (errors, dispatch) => 
+    dispatch(focus('updateTotal', Object.keys(errors)[0])),
+  onSubmitSuccess: (result, dispatch) => dispatch(reset('updateTotal'))
+})(AddToTotalForm);
+
+//export default connect(null, { increaseTotal, minusTotal })(AddToTotalForm);
