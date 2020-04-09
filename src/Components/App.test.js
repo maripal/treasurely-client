7134be5049244e58b1cd5b28988f9d1b8e2d277f
Maria Palacios <imarip09@gmail.com>
2020-04-09 14:22:14 -0500

    basic render test

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { App } from './App';
+import { Nav } from './Nav';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
