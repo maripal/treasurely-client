a8e6df27c2bd170f7b5999a898b48eab58f4595f
Maria Palacios <imarip09@gmail.com>
2020-04-21 11:29:29 -0500

    fixed jsx structure and added styles so item content aligns correctly

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Item.css';
 
-const Item = props => {
+export const Item = props => {
   return (
-    <div>
-      <h4 className="item-name">{props.name}</h4>
-      <p className="price">${props.price.toFixed(2)}</p>
-      <button className={`item-action-button ${props.className}`} onClick={() => props.buyClick(props.price, props.id)}>Buy</button>
-      <button className="item-action-button" onClick={() => props.editClick(props.id)}>Edit</button>
-      <button className="item-action-button" onClick={() => props.clickDelete(props.id)}>Delete</button>
+    <div className="item-content">
+      <div className="item-details">
+        <h4 className="item-name">{props.name}</h4>
+        <p className="price">${props.price.toFixed(2)}</p>
+      </div>
+      <button className={`item-action-button buy-button ${props.className}`} onClick={() => props.buyClick(props.price, props.id)}>Buy</button>
+      <button className="item-action-button edit-button" onClick={() => props.editClick(props.id)}>Edit</button>
+      <button className="item-action-button delete-button" onClick={() => props.clickDelete(props.id)}>Delete</button>
     </div>
   );
 };
