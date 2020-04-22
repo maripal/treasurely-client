16d3b139d549f69f01e8445d0f97a53305a4141a
Maria Palacios <imarip09@gmail.com>
2020-04-22 16:15:53 -0500

    deleted style objects and added classnames for css file instead

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,56 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './Modal.css';
+
 const Modal = props => {
   console.log(props);
-  const modalStyle =  {
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    position: 'fixed',
-    height: '100%',
-    width: '100%',
-    top: '0',
-    left: '0',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-  };
-
-  const innerElementStyle = {
-    backgroundColor: 'white',
-    width: '100%',
-    height: '60vh',
-    padding: '15px',
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center'
-  }
-
-  const formTitleStyle = {
-    textAlign: 'center',
-  };
-
-  const closeButton = {
-    position: 'absolute',
-    top: '10px',
-    right: '5px',
-    fontSize: '18px',
-    fontWeight: '700'
-  }
-
   //FIX FORM STYLING!
 
   //props is coming from our 'addItemForm' component.
    return ReactDOM.createPortal(
-    <div style={modalStyle}>
-      <div style={innerElementStyle}>
-        <div style={formTitleStyle}>
+    <div className="modal-container">
+      <div className="modal-content">
+        <h3 className="modal-title">
           {props.title}
-        </div>
-        <div>
+        </h3>
+        <div className="modal-form">
           {props.children}
         </div>
-        <button style={closeButton} onClick={props.hideModal}>x</button>
+        <button className="close-btn" onClick={props.hideModal}>x</button>
       </div>
     </div>,
     document.getElementById('modal')
