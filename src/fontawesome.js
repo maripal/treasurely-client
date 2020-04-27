5a09c4adc44fc98d31a1184c468c1f16839daca0
Maria Palacios <imarip09@gmail.com>
2020-04-27 15:29:50 -0500

    added fontawesome icon library and icons needed

---
+++ src/fontawesome.js
@@ -0,0 +1,17 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+  faDollarSign,
+  faListUl,
+  faTasks,
+  faPencilAlt,
+  faTrashAlt
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faDollarSign,
+  faListUl,
+  faTasks,
+  faPencilAlt,
+  faTrashAlt
+);
