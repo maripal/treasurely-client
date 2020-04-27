88f7e7268c895333f4120a40569ac7b3652dca58
Maria Palacios <imarip09@gmail.com>
2020-04-27 16:26:52 -0500

    added section with cards and styling

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -3,6 +3,8 @@ import { Link, Redirect } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import './LandingPage.css';
 import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 export function LandingPage(props) {
   
@@ -26,6 +28,33 @@ export function LandingPage(props) {
           <h3 className="log-in-title">Log In</h3>
           <LoginForm />
         </div>
+
+        <section className="card-container">
+          <div className="card">
+            <FontAwesomeIcon className="card-icon" icon="dollar-sign" />
+            <div className="card-content">
+              <h5 className="card-title">Track your savings</h5>
+              <p>Track your savings.</p>
+            </div>
+          </div>
+          <div className="card">
+            <FontAwesomeIcon className="card-icon" icon="list-ul" />
+            <div className="card-content">
+              <h5 className="card-title">Add an item to your list</h5>
+              <p>You can add anything that you wish to buy. It's your own wishlist.</p>
+            </div>
+          </div>
+          <div className="card">
+            <FontAwesomeIcon className="card-icon" icon="tasks" />
+            <div className="card-content">
+              <h5 className="card-title">Buy item</h5>
+              <p>When you have enough in your savings to buy a certain item, 
+                it'll notify you by changing the 'buy' button color for that item. 
+                If you decide to buy it, you can just check it off your list.
+              </p>
+            </div>
+          </div>
+        </section>
       </header>
     );
 };
