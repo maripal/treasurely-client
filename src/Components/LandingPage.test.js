eeda4549abfaac853a7ee9e7992c7a742be76e5a
Maria Palacios <imarip09@gmail.com>
2020-05-02 18:54:23 -0500

    added test

--- src/Components/LandingPage.test.js
+++ src/Components/LandingPage.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { LandingPage } from './LandingPage';
 
+
 describe('<LandingPage />', () => {
   it('renders without crashing', () => {
     shallow(<LandingPage />);
